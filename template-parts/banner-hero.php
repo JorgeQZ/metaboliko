eb56fde37a63f24fbca72f4c9eebbea3509a9160
Akevia <akevia@Akevias-iMac.local>
2020-03-12 11:48:54 -0600

    banner hero options

--- template-parts/banner-hero.php
+++ template-parts/banner-hero.php
@@ -1,12 +1,46 @@
 <?php $img_featured = get_the_post_thumbnail_url();?>
+<?php $post_type = get_post_type(); ?>
+
 <div class="hero-banner" style="background-image: url(<?php echo $img_featured; ?>">
+
+    <?php if($post_type == 'post'): ?>
     <!-- Titulo -->
     <div class="title">
         <?php the_title(); ?>
     </div><!-- Titulo -->
 
-       <!-- social-media -->
-       <div class="social-media">
+    <?php else: ?>
+
+    <?php 
+    $contenido = get_field('contenido'); 
+    switch($contenido):
+        case "Titulo": 
+                ?>
+                <!-- Titulo -->
+                <div class="title">
+                        <?php the_title(); ?>
+                </div><!-- Titulo -->
+                <?php
+                break;
+        case "Texto":
+                ?>
+                <!-- Titulo -->
+                <div class="content_text">
+                       <?php echo get_field('texto'); ?>
+                </div><!-- Titulo -->
+                <?php  
+                break;
+
+        case "Imagen y Texto":
+                break;
+    endswitch; 
+    ?>
+
+    <?php endif; ?>
+
+
+    <!-- social-media -->
+    <div class="social-media">
         <div class="links">
             <a href="#" target="_blank"><img
                     src="<?php echo get_template_directory_uri().'/img/icons/facebook-white.png'; ?>" alt=""></a>
