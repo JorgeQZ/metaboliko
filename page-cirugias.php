5162adeb46fb7ac202271d38756ce8fe2072070c
Byron-Guevara <byronguevara@akevia.mx>
2020-03-13 10:57:47 -0600

    Testimonios, Cirugias y Sistema

---
+++ page-cirugias.php
@@ -0,0 +1,277 @@
+<?php
+/**
+* Template Name: Cirugias
+* 
+*/
+?>
+<?php get_header(); ?>
+
+<!-- Hero Banner -->
+<div class="hero-banner">
+    <!-- hero title -->
+    <div class="hero-title">#RecuperaTuVida</div><!-- hero title -->
+
+    <!-- ring-overlay -->
+    <!--
+    <div class="ring-overlay"></div>
+    ring-overlay 
+-->
+
+    <!-- content -->
+<!--
+    <div class="content">
+        <div class="title">Quiero...</div>
+        <select name="hero-select" id="hero-select" class="select-custom">
+            <option value="Cirugías para Bajar de Peso">Cirugías para Bajar de Peso</option>
+            <option value="Cirugía Antirreflujo por Laparoscopia">Cirugía Antirreflujo por Laparoscopia</option>
+            <option value="Colecistectomía por Laparoscopía">Colecistectomía por Laparoscopía</option>
+            <option>Hernia Abdominal por Laparoscopia</option>
+            <option>Hernia Inguinal por Laparoscopia</option>
+        </select>
+    </div>
+-->
+    <!-- content -->
+
+    <!-- whatsapp button -->
+<!--
+    <div class="button-whatsapp">
+        <a href="#" target="_blank">
+            <img src="<?php echo get_template_directory_uri().'/img/whatsapp-button.png'; ?>" alt="">
+        </a>
+    </div>
+-->
+    <!-- whatasapp button-->
+
+    <!-- social-media -->
+    <div class="social-media">
+        <div class="links">
+            <a href="#" target="_blank"><img
+                    src="<?php echo get_template_directory_uri().'/img/icons/facebook-white.png'; ?>" alt=""></a>
+            <a href="#" target="_blank"><img
+                    src="<?php echo get_template_directory_uri().'/img/icons/twitter-white.png'; ?>" alt=""></a>
+            <a href="#" target="_blank"><img
+                    src="<?php echo get_template_directory_uri().'/img/icons/linkedin-white.png'; ?>" alt=""></a>
+            <a href="#" target="_blank"><img
+                    src="<?php echo get_template_directory_uri().'/img/icons/instagram-white.png'; ?>" alt=""></a>
+            <a href="#" target="_blank"><img
+                    src="<?php echo get_template_directory_uri().'/img/icons/youtube-white.png'; ?>" alt=""></a>
+            <a href="#" target="_blank"><img
+                    src="<?php echo get_template_directory_uri().'/img/icons/pinterest-white.png'; ?>" alt=""></a>
+        </div>
+    </div><!-- social-media -->
+    <!-- carousel -->
+    <div class="banner-carousel owl-carousel owl-theme">
+        <div class="item"
+            style="background-image: url(<?php echo get_template_directory_uri().'/img/banner-cirugias.jpg'; ?>)">
+            <!-- overlay -->
+            <div class="overlay"></div>
+            <!-- overlay -->
+        </div>
+    </div><!-- carousel -->
+
+</div><!-- Hero Banner -->
+
+<div class="contenedor-items-global">
+
+    <div class="contenedor-items-slider">
+        <div class="item-slider" style="background-image: url(<?php echo get_template_directory_uri().'/img/item-slidercirugias.jpg'; ?>);">
+            <p>
+                Banda <br> <span>Gástrica</span>
+            </p>
+        </div>
+        <div class="item-slider" style="background-image: url(<?php echo get_template_directory_uri().'/img/item2-slidercirugias.jpg'; ?>);">
+            <p>
+                Manga <br> <span>Gástrica</span>
+            </p>
+        </div>
+         <div class="item-slider" style="background-image: url(<?php echo get_template_directory_uri().'/img/item3-slidercirugias.jpg'; ?>);">
+            <p>
+                Bypass <br> <span>Gástrico</span>
+            </p>
+        </div>
+        <div class="item-slider" style="background-image: url(<?php echo get_template_directory_uri().'/img/item4-slidercirugias.jpg'; ?>);">
+            <p>
+                Cirugía de <br> <span>Revisión</span>
+            </p>
+        </div>
+    </div>
+
+    <div class="contenedor-item-global active">
+        <div class="contenedor-general-cirugias">
+            <div class="slider-cirugias">
+                <div class="cont-img">
+                    <div class="img">
+                        <img src="<?php echo get_template_directory_uri().'/img/img-cirugia.jpg'; ?>" alt="">
+                    </div>
+                </div>
+                <div class="cont-info">
+                    <p class="enc">
+                        <small>Banda</small> <br> Gástrica
+                        <span>
+                            <small>Banda</small> <br> Gástrica
+                        </span>
+                    </p>
+                    <p class="desc">
+                    Es una cirugía sencilla, segura, que hace que el paciente coma poco, se llene rápido y pierda peso. 
+                    En la acutalidad no es una cirugía que se haga constantemente, pero tiene algunas indiciaciones. 
+                    Es importante saber que los pacientes que estan operados de Banga Gástrica deben estar evaluados, 
+                    tener seguimiento y estudios por ejemplo endoscopia pues el riesgo de reflujo es alto y 
+                    con esto vienen algunos otros problemas.
+                    <br> <br>
+                    La banda gástrica por laparoscopia es una cirugía para bajar de peso que se lleva a cabo mediante tres 
+                    incisiones en el abdomen mediante los cuales se realiza la disección del estómago y se coloca la 
+                    banda gástrica, la cual rodea la parte superior de todo el estómago.
+                    </p>
+                </div>
+            </div>
+        </div>
+
+
+        <div class="contenedor-general-video">
+            <div class="contenedor-video">
+                <div id="slider-videos" class="owl-carousel owl-theme slider-videos">
+                    <div class="cont-item">
+                        <div class="media">
+                            <img src="<?php echo get_template_directory_uri().'/img/cirugias-video.jpg'; ?>" alt="">
+                        </div>
+                        <div class="info">
+                            <p>
+                            La banda gástrica por laparoscopia es una cirugía para bajar de peso 
+                            que se lleva a cabo mediante tres incisiones en el abdomen mediante 
+                            los cuales se realiza la disección del estómago y se coloca la banda 
+                            gástrica, la cual rodea la parte superior de todo el estómago.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="cont-item">
+                        <div class="media">
+                            <img src="<?php echo get_template_directory_uri().'/img/cirugias-video.jpg'; ?>" alt="">
+                        </div>
+                        <div class="info">
+                            <p>
+                            La banda gástrica por laparoscopia es una cirugía para bajar de peso 
+                            que se lleva a cabo mediante tres incisiones en el abdomen mediante 
+                            los cuales se realiza la disección del estómago y se coloca la banda 
+                            gástrica, la cual rodea la parte superior de todo el estómago.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+    <div class="contenedor-item-global">
+        <div class="contenedor-general-cirugias">
+            <div class="slider-cirugias">
+                <div class="cont-img">
+                    <div class="img">
+                        <img src="<?php echo get_template_directory_uri().'/img/img-cirugia.jpg'; ?>" alt="">
+                    </div>
+                </div>
+                <div class="cont-info">
+                    <p class="enc">
+                        <small>Manga</small> <br> Gástrica
+                        <span>
+                            <small>Manga</small> <br> Gástrica
+                        </span>
+                    </p>
+                    <p class="desc">
+                    Es una cirugía sencilla, segura, que hace que el paciente coma poco, se llene rápido y pierda peso. 
+                    En la acutalidad no es una cirugía que se haga constantemente, pero tiene algunas indiciaciones. 
+                    Es importante saber que los pacientes que estan operados de Banga Gástrica deben estar evaluados, 
+                    tener seguimiento y estudios por ejemplo endoscopia pues el riesgo de reflujo es alto y 
+                    con esto vienen algunos otros problemas.
+                    <br> <br>
+                    La banda gástrica por laparoscopia es una cirugía para bajar de peso que se lleva a cabo mediante tres 
+                    incisiones en el abdomen mediante los cuales se realiza la disección del estómago y se coloca la 
+                    banda gástrica, la cual rodea la parte superior de todo el estómago.
+                    </p>
+                </div>
+            </div>
+        </div>
+
+
+        <div class="contenedor-general-video">
+            <div class="contenedor-video">
+                <div id="slider-videos" class="owl-carousel owl-theme slider-videos">
+                    <div class="cont-item">
+                        <div class="media">
+                            <img src="<?php echo get_template_directory_uri().'/img/cirugias-video.jpg'; ?>" alt="">
+                        </div>
+                        <div class="info">
+                            <p>
+                            La manga gástrica por laparoscopia es una cirugía para bajar de peso 
+                            que se lleva a cabo mediante tres incisiones en el abdomen mediante 
+                            los cuales se realiza la disección del estómago y se coloca la banda 
+                            gástrica, la cual rodea la parte superior de todo el estómago.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="cont-item">
+                        <div class="media">
+                            <img src="<?php echo get_template_directory_uri().'/img/cirugias-video.jpg'; ?>" alt="">
+                        </div>
+                        <div class="info">
+                            <p>
+                            La manga gástrica por laparoscopia es una cirugía para bajar de peso 
+                            que se lleva a cabo mediante tres incisiones en el abdomen mediante 
+                            los cuales se realiza la disección del estómago y se coloca la banda 
+                            gástrica, la cual rodea la parte superior de todo el estómago.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<?php get_footer(); ?>
+
+<script>
+
+$(document).ready(function() {
+    $(".banner-carousel").owlCarousel({
+        animateOut: 'slideOutLeft',
+        animateIn: 'slideInRight',
+        items: 1,
+        margin: 0,
+        stagePadding: 0,
+        smartSpeed: 450,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 9000,
+        autoplayHoverPause: true
+    });
+
+    $(".slider-videos").owlCarousel({
+        animateOut: 'slideOutLeft',
+        animateIn: 'slideInRight',
+        items: 1,
+        margin: 0,
+        stagePadding: 0,
+        smartSpeed: 450,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 9000,
+        nav:true,
+        navText : ["<img src='<?php echo get_template_directory_uri().'/img/arrowl-azul-video.png'; ?>'>","<img src='<?php echo get_template_directory_uri().'/img/arrowr-azul-video.png'; ?>'>"],
+        dots: false,
+        autoplayHoverPause: true
+    });
+
+
+    $(".contenedor-items-global .contenedor-items-slider .item-slider").each(function(index){
+        $(this).click(function(){
+            $(".contenedor-item-global").removeClass("active");
+            $(".contenedor-item-global").eq(index).addClass("active");
+        });
+    });
+
+
+});
+</script>
---
+++ page-sistema.php
@@ -0,0 +1,309 @@
+<?php
+/**
+* Template Name: Sistema
+* 
+*/
+?>
+<?php get_header(); ?>
+
+<!-- Hero Banner -->
+<div class="hero-banner">
+    <!-- hero title -->
+    <div class="hero-title">#RecuperaTuVida</div><!-- hero title -->
+
+    <!-- ring-overlay -->
+    <!--
+    <div class="ring-overlay"></div>
+    ring-overlay 
+-->
+
+    <!-- content -->
+<!--
+    <div class="content">
+        <div class="title">Quiero...</div>
+        <select name="hero-select" id="hero-select" class="select-custom">
+            <option value="Cirugías para Bajar de Peso">Cirugías para Bajar de Peso</option>
+            <option value="Cirugía Antirreflujo por Laparoscopia">Cirugía Antirreflujo por Laparoscopia</option>
+            <option value="Colecistectomía por Laparoscopía">Colecistectomía por Laparoscopía</option>
+            <option>Hernia Abdominal por Laparoscopia</option>
+            <option>Hernia Inguinal por Laparoscopia</option>
+        </select>
+    </div>
+-->
+    <!-- content -->
+
+    <!-- whatsapp button -->
+<!--
+    <div class="button-whatsapp">
+        <a href="#" target="_blank">
+            <img src="<?php echo get_template_directory_uri().'/img/whatsapp-button.png'; ?>" alt="">
+        </a>
+    </div>
+-->
+    <!-- whatasapp button-->
+
+    <!-- social-media -->
+    <div class="social-media">
+        <div class="links">
+            <a href="#" target="_blank"><img
+                    src="<?php echo get_template_directory_uri().'/img/icons/facebook-white.png'; ?>" alt=""></a>
+            <a href="#" target="_blank"><img
+                    src="<?php echo get_template_directory_uri().'/img/icons/twitter-white.png'; ?>" alt=""></a>
+            <a href="#" target="_blank"><img
+                    src="<?php echo get_template_directory_uri().'/img/icons/linkedin-white.png'; ?>" alt=""></a>
+            <a href="#" target="_blank"><img
+                    src="<?php echo get_template_directory_uri().'/img/icons/instagram-white.png'; ?>" alt=""></a>
+            <a href="#" target="_blank"><img
+                    src="<?php echo get_template_directory_uri().'/img/icons/youtube-white.png'; ?>" alt=""></a>
+            <a href="#" target="_blank"><img
+                    src="<?php echo get_template_directory_uri().'/img/icons/pinterest-white.png'; ?>" alt=""></a>
+        </div>
+    </div><!-- social-media -->
+    <!-- carousel -->
+    <div class="banner-carousel owl-carousel owl-theme">
+        <div class="item"
+            style="background-image: url(<?php echo get_template_directory_uri().'/img/banner-sistema.jpg'; ?>)">
+            <!-- overlay -->
+            <div class="overlay"></div>
+            <!-- overlay -->
+        </div>
+    </div><!-- carousel -->
+
+</div><!-- Hero Banner -->
+
+<div class="contenedor-general-tenedor">
+    <img src="<?php echo get_template_directory_uri().'/img/fondo-tenedor.jpg'; ?>" alt="">
+
+<div class="contenedor-items-global">
+    <div class="contenedor-item-global active">
+        <div class="contenedor-info">
+            <p class="enc">
+                    <small>Sistema</small> <br> Proteinado
+                    <span>
+                        <small>Sistema</small> <br> Proteinado
+                    </span>
+                </p>
+            <p class="desc">
+                El Sistema Proteinado es un tratamiento médico estricto, en el cual los pacientes 
+                entran en un estado metabólico controlado bajo estricta supervisión médica, 
+                logrando que su cuerpo gaste la GRASA almacenada en su cuerpo como fuente de energía. 
+                <br> <br>
+                Este cambio metabólico tiene como fin que el paciente pierda peso sin tener hambre, 
+                pero lo más importante viene después, ya que el paciente debe aprender a comer en 
+                forma intuitiva, es decir, en base a lo que haga en su día a día y en este proceso 
+                subir masa muscular con el fin de que el músculo le permita quemar más calorías 
+                y sea mas facíl mantener su peso ideal.
+            </p>
+        </div>
+        
+        <div class="contenedor-general-videos">
+            <div class="contenedor-video">
+                <div id="slider-videos" class="owl-carousel owl-theme slider-videos">
+                    <div class="cont-item">
+                        <div class="media">
+                            <img src="<?php echo get_template_directory_uri().'/img/sistema-video.jpg'; ?>" alt="">
+                        </div>
+                        <div class="info">
+                            <p>
+                            En resumen el SISTEMA PROTEINADO logra hacer que el paciente pierda grasa sin tener hambre para después 
+                            aprender a comer en forma intuitiva mientras sube su masa muscular en las areas que mayormente le benefician, 
+                            por ejemplo en una mujer pierna y nalga.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="cont-item">
+                        <div class="media">
+                            <img src="<?php echo get_template_directory_uri().'/img/sistema-video.jpg'; ?>" alt="">
+                        </div>
+                        <div class="info">
+                            <p>
+                            En resumen el SISTEMA PROTEINADO logra hacer que el paciente pierda grasa sin tener hambre para después 
+                            aprender a comer en forma intuitiva mientras sube su masa muscular en las areas que mayormente le benefician, 
+                            por ejemplo en una mujer pierna y nalga.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="contenedor-item-global">
+        <div class="contenedor-info">
+            <p class="enc">
+                    <small>Masa</small> <br> Muscular
+                    <span>
+                        <small>Masa</small> <br> Muscular
+                    </span>
+                </p>
+            <p class="desc">
+                La masa muscular es un tratamiento médico estricto, en el cual los pacientes 
+                entran en un estado metabólico controlado bajo estricta supervisión médica, 
+                logrando que su cuerpo gaste la GRASA almacenada en su cuerpo como fuente de energía. 
+                <br> <br>
+                Este cambio metabólico tiene como fin que el paciente pierda peso sin tener hambre, 
+                pero lo más importante viene después, ya que el paciente debe aprender a comer en 
+                forma intuitiva, es decir, en base a lo que haga en su día a día y en este proceso 
+                subir masa muscular con el fin de que el músculo le permita quemar más calorías 
+                y sea mas facíl mantener su peso ideal.
+            </p>
+        </div>
+        
+        <div class="contenedor-general-videos">
+            <div class="contenedor-video">
+                <div id="slider-videos" class="owl-carousel owl-theme slider-videos">
+                    <div class="cont-item">
+                        <div class="media">
+                            <img src="<?php echo get_template_directory_uri().'/img/sistema-video.jpg'; ?>" alt="">
+                        </div>
+                        <div class="info">
+                            <p>
+                            En resumen la MASA MUSCULAR logra hacer que el paciente pierda grasa sin tener hambre para después 
+                            aprender a comer en forma intuitiva mientras sube su masa muscular en las areas que mayormente le benefician, 
+                            por ejemplo en una mujer pierna y nalga.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="cont-item">
+                        <div class="media">
+                            <img src="<?php echo get_template_directory_uri().'/img/sistema-video.jpg'; ?>" alt="">
+                        </div>
+                        <div class="info">
+                            <p>
+                            En resumen la MASA MUSCULAR logra hacer que el paciente pierda grasa sin tener hambre para después 
+                            aprender a comer en forma intuitiva mientras sube su masa muscular en las areas que mayormente le benefician, 
+                            por ejemplo en una mujer pierna y nalga.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+    <div class="contenedor-items-slider">
+        <div class="item-slider" style="background-image: url(<?php echo get_template_directory_uri().'/img/item-slidersistema.jpg'; ?>);">
+            <p>
+            Perder <br> <span>Peso</span>
+            </p>
+        </div>
+        <div class="item-slider" style="background-image: url(<?php echo get_template_directory_uri().'/img/item2-slidersistema.jpg'; ?>);">
+            <p>
+            Incremento de <br> <span>Masa Muscular</span>
+            </p>
+        </div>
+        <div class="item-slider" style="background-image: url(<?php echo get_template_directory_uri().'/img/item3-slidersistema.jpg'; ?>);">
+            <p>
+            Mejora de <br> <span>Imagen</span>
+            </p>
+        </div>
+        <div class="item-slider" style="background-image: url(<?php echo get_template_directory_uri().'/img/item4-slidersistema.jpg'; ?>);">
+            <p>
+            Definición <br> <span>Muscular</span>
+            </p>
+        </div>
+    </div>
+</div>
+
+
+<div class="contenedor-general-galeria">
+    <div class="cont-galeria">
+        <div id="slider-galeria" class="owl-carousel owl-theme">
+            <div class="cont-slide">
+                <div class="cont-img">
+                    <img src="<?php echo get_template_directory_uri().'/img/img-galeria.jpg'; ?>" alt="">
+                </div>
+                <div class="cont-img">
+                    <img src="<?php echo get_template_directory_uri().'/img/img-galeria2.jpg'; ?>" alt="">
+                </div>
+                <div class="cont-img">
+                    <img src="<?php echo get_template_directory_uri().'/img/img-galeria3.jpg'; ?>" alt="">
+                </div>
+                <div class="cont-img">
+                    <img src="<?php echo get_template_directory_uri().'/img/img-galeria.jpg'; ?>" alt="">
+                </div>
+                <div class="cont-img">
+                    <img src="<?php echo get_template_directory_uri().'/img/img-galeria2.jpg'; ?>" alt="">
+                </div>
+                <div class="cont-img">
+                    <img src="<?php echo get_template_directory_uri().'/img/img-galeria3.jpg'; ?>" alt="">
+                </div>
+                <div class="cont-img">
+                    <img src="<?php echo get_template_directory_uri().'/img/img-galeria.jpg'; ?>" alt="">
+                </div>
+                <div class="cont-img">
+                    <img src="<?php echo get_template_directory_uri().'/img/img-galeria2.jpg'; ?>" alt="">
+                </div>
+                <div class="cont-img">
+                    <img src="<?php echo get_template_directory_uri().'/img/img-galeria3.jpg'; ?>" alt="">
+                </div>
+                <div class="cont-img">
+                    <img src="<?php echo get_template_directory_uri().'/img/img-galeria.jpg'; ?>" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php get_footer(); ?>
+
+<script>
+
+$(document).ready(function() {
+    $(".banner-carousel").owlCarousel({
+        animateOut: 'slideOutLeft',
+        animateIn: 'slideInRight',
+        items: 1,
+        margin: 0,
+        stagePadding: 0,
+        smartSpeed: 450,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 9000,
+        autoplayHoverPause: true
+    });
+
+    
+
+    $(".slider-videos").owlCarousel({
+        animateOut: 'slideOutLeft',
+        animateIn: 'slideInRight',
+        items: 1,
+        margin: 0,
+        stagePadding: 0,
+        smartSpeed: 450,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 9000,
+        nav:true,
+        navText : ["<img src='<?php echo get_template_directory_uri().'/img/arrowl-verde-video.png'; ?>'>","<img src='<?php echo get_template_directory_uri().'/img/arrowr-verde-video.png'; ?>'>"],
+        dots: false,
+        autoplayHoverPause: true
+    });
+
+
+
+    $("#slider-galeria").owlCarousel({
+        animateOut: 'slideOutLeft',
+        animateIn: 'slideInRight',
+        items: 1,
+        margin: 0,
+        stagePadding: 0,
+        smartSpeed: 450,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 9000,
+        autoplayHoverPause: true
+    });
+
+
+    $(".contenedor-general-tenedor .contenedor-items-slider .item-slider").each(function(index){
+        $(this).click(function(){
+            $(".contenedor-item-global").removeClass("active");
+            $(".contenedor-item-global").eq(index).addClass("active");
+        });
+    });
+
+
+});
+</script>
