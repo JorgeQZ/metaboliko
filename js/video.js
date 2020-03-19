ac780a964d3ef1f0689835f34ff9ad78046cd4cc
Byron-Guevara <byronguevara@akevia.mx>
2020-03-19 10:35:27 -0600

    Testimonios autoadministrable completo, ya con videos

--- js/video.js
+++ js/video.js
@@ -26,6 +26,12 @@ $.fn.CustomVideo = function() {
 }
 
 $(document).ready(function() {
+
+    var navv = $(".video-carousel").attr("nav");
+    if(!navv){
+        navv = false;
+    }
+
     $(".video-carousel").owlCarousel({
         animateOut: 'slideOutLeft',
         animateIn: 'slideInRight',
@@ -35,7 +41,8 @@ $(document).ready(function() {
         center: true,
         lazyLoad: false,
         smartSpeed: 450,
-        onDragged: PauseVideos
+        onDragged: PauseVideos,
+        nav: navv
     });
 
     $(".customVideo").CustomVideo();
